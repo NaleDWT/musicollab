45ae7f80601e4c997b25160e5cb8c41577b067e2
NaleDWT <jonathan.maredsous@outlook.com>
2023-05-30 22:31:36 +0200

    section1to3

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,17 @@
-console.info('Hello world');
+console.info('coucou');
+
+  //Sticky menu
+
+  const nav = document.querySelector(".menu");
+  const header = document.querySelector(".headcontainer");
+  const sticky = nav.offsetTop;
+  window.onscroll = function() {sticker()};
+  function sticker() {
+     if (window.pageYOffset >= sticky) {
+        nav.classList.add("sticky")
+        header.classList.add("sticky")
+     } else {
+        nav.classList.remove("sticky");
+        header.classList.remove("sticky");
+     }
+  }
